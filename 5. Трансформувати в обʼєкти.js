4bc3bbd5720f090a4dc9f31a0b4cb1c0f44d23cc
Levko <ganchuklevko@gmail.com>
2025-03-24 17:10:58 +0200

    Update 5. Трансформувати в обʼєкти.js

--- 5. Трансформувати в обʼєкти.js
+++ 5. Трансформувати в обʼєкти.js
@@ -4,17 +4,10 @@ let mariya = { name: "Марія", surname: "Мрійко", id: 3 };
 
 let users = [ ivan, petro, mariya ];
 
-let usersMapped = users.map( users => { 
-    return {
-        fullName: `${users.name} ${users.surname}`,
-        id: users.id
-    }
- } );
+let usersMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+}));
 
 alert( usersMapped[0].id ) // 1
 alert( usersMapped[0].fullName ) // Іван Іванко
-
-//з цим завданням я спитався підказки ChatGPT, 
-//бо не знав взагалі, що пілся .map можна робити функцію.
-//і про `${} цю штуку, забув, це з html взагалі
-// але так то завдання зрозумів
